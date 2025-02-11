7eca4b45efff6e284ea0e9d0e4a18353e9cbfaa0
Vitoria Stoffel Covolan <vitoria.covolan@dnacenter>
2025-02-11 14:58:41 -0300

    feat: construtor de classe

--- restaurantes.py
+++ restaurantes.py
@@ -1,10 +1,11 @@
 class Resturante:
-    nome = ""
-    categoria = ""
-    ativo = True    
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = True    
 
-restaurante_praca = Resturante()
-restaurante_pizza = Resturante()
+restaurante_praca = Resturante('Cadu', 'sushi')
+restaurante_pizza = Resturante('Vitoria', 'Poke')
 
 restaurantes = [restaurante_praca, restaurante_pizza]
-print(dir(restaurante_praca))
+print(vars(restaurante_praca))
