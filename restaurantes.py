7eab68ca3b0862e4b40bcbc82d40a165d94c8321
Vitoria Stoffel Covolan <vitoria.covolan@dnacenter>
2025-02-12 10:34:21 -0300

    feat: uso de str e função para listar restaurentes

--- restaurantes.py
+++ restaurantes.py
@@ -1,11 +1,19 @@
-class Resturante:
+class Restaurante:
+    restaurantes = []
+
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
         self.ativo = True    
+        Restaurante.restaurantes.append(self)    
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
+    
+    def listar_restaurantes(self):
+        for r in Restaurante.restaurantes:
+            print(f'{r.nome} | {r.categoria} | {r.ativo}')
 
-restaurante_praca = Resturante('Cadu', 'sushi')
-restaurante_pizza = Resturante('Vitoria', 'Poke')
+restaurante_praca = Restaurante('Cadu', 'sushi')
+restaurante_pizza = Restaurante('Vitoria', 'Poke')
 
-restaurantes = [restaurante_praca, restaurante_pizza]
-print(vars(restaurante_praca))
+Restaurante.listar_restaurantes(Restaurante)
