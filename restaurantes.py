28d09aea624b385dd39c8aba5ebc9faaf9d08294
Vitoria Stoffel Covolan <vitoria.covolan@dnacenter>
2025-02-12 10:51:14 -0300

    feat: melhorando a visualizaçãoe usando property

--- restaurantes.py
+++ restaurantes.py
@@ -4,14 +4,19 @@ class Restaurante:
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = True    
+        self._ativo = True    #colocar o _ faz com que seja um atributo provado e as pessoas não conseguem acessar ele direto
         Restaurante.restaurantes.append(self)    
     def __str__(self):
         return f'{self.nome} | {self.categoria}'
     
     def listar_restaurantes(self):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | Ativo')
         for r in Restaurante.restaurantes:
-            print(f'{r.nome} | {r.categoria} | {r.ativo}')
+            print(f'{r.nome.ljust(25)} | {r.categoria.ljust(25)} | {r.ativo}')
+
+    @property
+    def ativo(self):
+        return '☑' if self._ativo else   '☐'
 
 restaurante_praca = Restaurante('Cadu', 'sushi')
 restaurante_pizza = Restaurante('Vitoria', 'Poke')
